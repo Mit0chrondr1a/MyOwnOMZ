e15f49c02372d7a5547a5af0a18f4fcd95e6fc99
Mit0chrondr1a <root@VM5163AD5579BEE80.local>
2024-01-10 18:34:07 +0800

    cherry-pick alias.zsh from branches

--- alias.zsh
+++ alias.zsh
@@ -1,13 +1,25 @@
+
+# general part
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
+alias cdsk='cd $(sk)'
+
 #eza enabled, comment out ls related aliases
 #alias ll='ls -l'
 #alias lh='ls -lh'
 #alias la='ls -a'
 #alias lal='ls -al'
 
-alias firewalld='firewall-cmd'
-alias ssctl='systemctl'
-alias firehow='firewalld --help | grep'
-alias firemod='firewalld --zone=public'
+# apt related
+alias apu='sudo apt update && sudo apt upgrade -y'
+alias api='sudo apt install -y'
+
+# homebrew related
+alias brr='brew remove'
+alias bi='brew install'
+
+# git related
+alias gca='git commit --amend'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
