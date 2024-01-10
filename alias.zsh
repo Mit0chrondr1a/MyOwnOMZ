984ce9ac2aa702e8e2432d9c92994f940a58a0ae
Mit0chrondr1a <12169214+Mit0chrondr1a@users.noreply.github.com>
2024-01-10 19:30:35 +0800

    Provide a new branch for initialising usecase

--- alias.zsh
+++ alias.zsh
@@ -1,13 +1,27 @@
+
+# general part
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
+alias cdsk='cd $(sk)'
+
 #eza enabled, comment out ls related aliases
 #alias ll='ls -l'
 #alias lh='ls -lh'
 #alias la='ls -a'
 #alias lal='ls -al'
 
+# RHEL specific
 alias firewalld='firewall-cmd'
 alias ssctl='systemctl'
 alias firehow='firewalld --help | grep'
 alias firemod='firewalld --zone=public'
+
+# homebrew related
+alias brr='brew remove'
+alias bi='brew install'
+
+# git related
+alias gca='git commit --amend'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
