40a922ccc7ba7c45309195a4103dde847956293f
Ryanng <ryanngxm@gmail.com>
2024-01-10 18:09:45 +0800

    add git related aliases

--- alias.zsh
+++ alias.zsh
@@ -1,11 +1,21 @@
+
+# general part
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
+alias cdsk='cd $(sk)'
+
 #eza enabled, comment out ls related aliases
 #alias ll='ls -l'
 #alias lh='ls -lh'
 #alias la='ls -a'
 #alias lal='ls -al'
 
-alias cdsk='cd $(sk)'
+# homebrew related
+alias brr='brew remove'
 alias bi='brew install'
+
+# git related
+alias gca='git commit --amend'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
