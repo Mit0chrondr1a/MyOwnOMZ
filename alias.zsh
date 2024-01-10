94dfc3b268763c9ac4c41a4dd8feb082fa6f7924
Ryanng <ryanngxm@gmail.com>
2024-01-10 17:34:43 +0800

    tidy up with comments in alias.zsh

--- alias.zsh
+++ alias.zsh
@@ -1,13 +1,25 @@
+
+# general part
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
+alias cdsk='cd $(sk)'
+
 #eza enabled, comment out ls related aliases
 #alias ll='ls -l'
 #alias lh='ls -lh'
 #alias la='ls -a'
 #alias lal='ls -al'
 
-alias cdsk='cd $(sk)'
-alias bi='brew install'
+# apt related
 alias apu='sudo apt update && sudo apt upgrade -y'
-alias api='sudo apt install'
+alias api='sudo apt install -y'
+
+# homebrew related
+alias brr='brew remove'
+alias bi='brew install'
+
+# git related
+alias gca='git commit --amend'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
